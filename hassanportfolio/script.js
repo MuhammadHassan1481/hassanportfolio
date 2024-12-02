499917f30334daac9c2f3f469c2d49b37ebe06ed
MuhammadHassan1481 <awaisawais00011@gmail.com>
2024-12-02 16:04:00 +0500

    Add files via upload

---
+++ hassanportfolio/script.js
@@ -0,0 +1,5 @@
+// Toggle Navbar
+document.getElementById('menu-toggle').addEventListener('click', () => {
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('hidden');
+});
